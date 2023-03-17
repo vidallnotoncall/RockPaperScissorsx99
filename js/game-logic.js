cd1ddbcdb5fd938d18010b7ecef4582493ff1624
Vidall <vidall.williams.web@gmail.com>
2023-03-16 23:07:04 -0400

    code for functions started

---
+++ js/game-logic.js
@@ -0,0 +1,30 @@
+// All code should be written in this file.
+// global move and value variables
+let playerOneMoveOneType,
+playerOneMoveTwoType,
+playerOneMoveThreeType,
+playerOneMoveOneValue,
+playerOneMoveTwoValue,
+playerOneMoveThreeValue,
+playerTwoMoveOneType,
+playerTwoMoveTwoType,
+playerTwoMoveThreeType,
+playerTwoMoveOneValue,
+playerTwoMoveTwoValue,
+playerTwoMoveThreeValue;
+
+const setPlayerMoves = () =>{
+
+}
+
+const getRoundWinner = () =>{
+
+}
+
+const getGameWinner = () => {
+
+}
+
+const setComputerMoves = () =>{
+  
+}
