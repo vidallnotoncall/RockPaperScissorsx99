9fc5e7747378e99b1fb16d15df0f5b766f546b23
Vidall <vidall.williams.web@gmail.com>
2023-04-06 23:29:11 -0400

    finished main logic for variable assignment

--- js/game-logic.js
+++ js/game-logic.js
@@ -14,15 +14,112 @@ playerTwoMoveTwoValue,
 playerTwoMoveThreeValue;
 
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) =>{
+  if (player = "Player One"){
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeValue = moveThreeValue;
+  }
 
+  if (player = "Player Two"){
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
 }
 
 const getRoundWinner = (roundNumber) =>{
-  
+  let playerOnePoints = 0;
+  let playerTwoPoints = 0;
+
+  switch (roundNumber){
+    case 1:
+      if (playerOneMoveOneType === playerTwoMoveOneType){
+        if(PlayerOneMoveOneValue === playerOneMoveOneType){
+          return "tie";
+        }
+        if (playerOneMoveOneType === "Rock" && playerTwoMoveOneValue === "Paper"){
+          return "Player Two";
+        } 
+        else if (playerOneMoveOneType === "Paper" && playerTwoMoveOneValue === "Scissors"){
+          return "Player Two";
+        }
+
+        }
+      }
+
+      case 2:
+        if (playerOneMoveTwoType === playerTwoMoveTwoType){
+          if(PlayerOneMoveTwoValue === playerOneMoveTwoType){
+            return "tie";
+          }
+          if (playerOneMoveTwoType > playerTwoMoveTwoValue){
+            return "Player One";
+          } else {
+            return "Player Two";
+          }
+        }
+      
+      case 3:
+        if (playerOneMoveThreeType === playerTwoMoveThreeType){
+          if(PlayerOneMoveThreeValue === playerOneMoveThreeType){
+            return "tie";
+          }
+          if (playerOneMoveThreeType > playerTwoMoveThreeValue){
+            return "Player One";
+          } else {
+            return "Player Two";
+          }
+        }        
+  }
 }
 
 const getGameWinner = () => {
-  let total = 0;
+  let playerOneTotalWins = 0;
+  let playerTwoTotalWins = 0;
+
+  const roundOneWinner = getRoundWinner(1);
+  const roundTwoWinner = getRoundWinner(2);
+  const roundThreeWinner = getRoundWinner(3);
+  if (roundOneWinner === "Player One"){
+    playerOneTotalWins += 1;
+  }else if(roundOneWinner === "Player Two"){
+    playerTwoTotalWins += 1;
+  }else{
+    playerOneTotalWins += 1;
+    playerTwoTotalWins +=1;
+  }
+
+    if (roundTwoWinner === "Player One") {
+      playerOneTotalWins += 1;
+    } else if (roundTwoWinner === "Player Two") {
+      playerTwoTotalWins += 1;
+    } else {
+      playerOneTotalWins += 1;
+      playerTwoTotalWins += 1;
+    }
+
+  if (roundThreeWinner === "Player One") {
+    playerOneTotalWins += 1;
+  } else if (roundThreeWinner === "Player Two") {
+    playerTwoTotalWins += 1;
+  } else {
+    playerOneTotalWins += 1;
+    playerTwoTotalWins += 1;
+  }
+
+  if (playerOneTotalWins > playerTwoTotalWins){
+    return "Player One";
+  } else if (playerOneTotalWins < playerTwoTotalWins) {
+    return "Player Two";
+  }else{
+    return "Tie";
+  }
 }
 
 const setComputerMoves = () =>{
